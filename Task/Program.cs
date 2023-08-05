c6028b3f0506276e6af4e9e6f7031265df6e244e
Alexandr <Sanya_1997_mai@mail.ru>
2023-08-05 20:43:07 +0300

    ReadInt

---
+++ Task/Program.cs
@@ -0,0 +1,11 @@
+﻿int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+//------------------------------------------------------------------------
+
+int N = ReadInt("Укажите количество элементов в массиве:");
+
