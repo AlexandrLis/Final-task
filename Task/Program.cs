095a33d9fcf31fcaf2a62c36e66162c5b567392d
Alexandr <Sanya_1997_mai@mail.ru>
2023-08-05 20:44:40 +0300

    FillArray and PrintArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,8 +4,28 @@
     return Convert.ToInt32(Console.ReadLine());
 }
 
+string [] FillArray (int N)
+{
+    string [] array = new string [N];
+    System.Console.WriteLine("Введите элементы массива:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = System.Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string [] array)
+{
+    System.Console.Write("[" + string.Join(", ", array) + "] -> ");
+}
 
 //------------------------------------------------------------------------
 
 int N = ReadInt("Укажите количество элементов в массиве:");
+
+string [] arr = FillArray(N);
+PrintArray(arr);
+
+
 
